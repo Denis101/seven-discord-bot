d5a332dc68418f4794be98f853247f17e17b471d
Denis Craig <admin@deniscraig.com>
2020-01-26 11:59:14 +0000

    I'm a 2019 guy

--- cmds/2019guy.js
+++ cmds/2019guy.js
@@ -2,5 +2,12 @@
 const State = require('../state.js');
 
 module.exports = () => {
+    const g = State.getClient().guilds.first();
+    const chan = g.channels.find(c => c.name === "Just Chatting");
+    if (!chan.joinable) {
+        State.getMessage().channel.send('I don\'t have permissions to join the channel :(');
+    }
+
+    chan.join();
     State.getMessage().channel.send('-play https://www.youtube.com/watch?v=4SiiRx7GDzI');
 };
